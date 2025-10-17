614ba002314697b42e36d2a5844dcdd0e6124c67
shaggy0397 <mariomoz.039709@gmail.com>
2025-10-17 04:53:14 -0600

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,33 @@
-const CACHE_NAME = 'hielo-cache-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+const CACHE_NAME = 'deimon-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html'
+];
+
+// Install event: Cache the application shell
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+
+// Fetch event: Serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // Not in cache - fetch from network
+        return fetch(event.request);
+      }
+    )
+  );
 });
+
